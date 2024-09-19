a7143211189d5459d3af70c3e16f0d89b41d48a5
Wojciech Duda <wo.duda@gmail.com>
2024-09-19 15:55:55 +0200

    Showing $_SERVER var

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,10 @@
 <h1>Welcome!</h1>
 <p>
     This is staring page of PHP Docker Example.<br />
-    Check PHP configuration there: <a href="php.php">php.php</a>
+    Check PHP configuration there: <a href="php.php">php.php</a><br />
+    $_SERVER VAR:<br />
 </p>
+
+<pre><?php
+    print_r($_SERVER);
+?></pre>
